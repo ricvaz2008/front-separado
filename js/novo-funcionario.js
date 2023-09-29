f09786fdab064d44a2ede58af38b246dd8d5f08e
ricvaz2008 <111580288+ricvaz2008@users.noreply.github.com>
2023-09-28 20:34:32 -0400

    Primeiro commit

---
+++ js/novo-funcionario.js
@@ -0,0 +1,44 @@
+let novoNome = document.getElementById("nomeFuncionario");
+let novoId = document.getElementById("idFuncionario");
+let novoCargo = document.getElementById("cargoFuncionario");
+let novoLogin = document.getElementById("loginFuncionario");
+let novaSenha = document.getElementById("senhaFuncionario");
+let novoAcesso = document.getElementById("acesso");
+
+function enviarMensagem(pedido) {
+  const url = 'http://localhost:3000/';
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(pedido)
+  });
+}
+
+function apagar() {
+  novoNome.value = "";
+  novoId.value = "";
+  novoCargo.value = "";
+  novoLogin.value = "";
+  novaSenha.value = "";
+}
+
+function confirmaCadastro() {
+  const pedido = {
+    action: "novoItemFuncionario",
+    id: novoId.value,
+    nome: novoNome.value,
+    cargo: novoCargo.value,
+    login: novoLogin.value,
+    senha: novaSenha.value,
+    acesso: novoAcesso.value,
+  };
+  enviarMensagem(pedido)
+    .then((resposta) => resposta.json())
+    .then(statusCadastro => {
+      if (statusCadastro.status == "itemCadastrado") {
+        apagar();
+      }
+    });
+}
