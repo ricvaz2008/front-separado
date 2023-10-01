32446c0a89869b08a4cf52c00ba45c38ba95f69c
ricvaz2008 <111580288+ricvaz2008@users.noreply.github.com>
2023-10-01 17:58:16 -0400

    Mudanca na porta de comunicacao

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const statusComunicacao = document.getElementById("erro_autorizacao");
 
 function receberResposta(pedido) {
   const queryParams = new URLSearchParams(pedido).toString();
-  const url = `http://localhost:3000?${queryParams}`;
+  const url = `http://https://brave-plant-0b3e4cc0f.3.azurestaticapps.net:80?${queryParams}`;
   return fetch(url, {
     method: 'GET',
     headers: {
