f32cfb3d23777d6b42ca04fa2286a0c852225976
ricvaz2008 <111580288+ricvaz2008@users.noreply.github.com>
2023-10-10 20:09:26 -0400

    Pequenas alteracoes na formatacao

--- js/novo-estoque.js
+++ js/novo-estoque.js
@@ -35,7 +35,6 @@ function confirmaCadastro() {
     const year = data.getFullYear();
     const month = (data.getMonth() + 1).toString().padStart(2, '0');
     const day = data.getDate().toString().padStart(2, '0');
-
     vencProduto.value = year + "-" + month + "-" + day;
 
     const pedido = {
